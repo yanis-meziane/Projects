39cc9dcc20524cffe84587e86569c7fa4fe2d26e
Yanis Meziane <yanismeziane04@icloud.com>
2026-01-03 14:16:54 +0100

    Ajout projet Java

---
+++ FireBones/composants/Personnage/Personnage.java
@@ -0,0 +1,98 @@
+public class Personnage {
+
+    protected String firstname;
+    protected String lastname;
+    protected int PV;
+    protected int PM;
+    protected int attaque;
+    protected int defense;
+    protected String Arme;
+
+    // Constructeur par défaut - REMPLACER LES VIRGULES PAR DES POINTS-VIRGULES
+    Personnage() {
+        this.firstname = "null";
+        this.lastname = "null";
+        this.PV = 0;
+        this.PM = 0;
+        this.attaque = 0;
+        this.defense = 0;
+        this.Arme = "null";
+    }
+
+    // Constructeur avec paramètres - REMPLACER LES VIRGULES PAR DES POINTS-VIRGULES
+    Personnage(String firstname, String lastname, int PV, int PM, int attaque, int defense, String Arme) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.PV = PV;
+        this.PM = PM;
+        this.attaque = attaque;
+        this.defense = defense;
+        this.Arme = Arme;
+    }
+
+    // SETTERS - doivent être VOID et MODIFIER la valeur (pas return)
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setPV(int PV) {
+        this.PV = PV;
+    }
+
+    public void setPM(int PM) {
+        this.PM = PM;
+    }
+
+    public void setAttaque(int attaque) {
+        this.attaque = attaque;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public void setArme(String Arme) {
+        this.Arme = Arme;
+    }
+
+    // GETTERS - OK sauf les fautes de frappe et types de retour
+    public String getFirstname() {
+        return this.firstname;
+    }
+
+    public String getLastname() {
+        return this.lastname;
+    }
+
+    public int getPV() {
+        return this.PV;
+    }
+
+    public int getPM() {
+        return this.PM;  // "retunr" corrigé
+    }
+
+    public int getAttaque() {
+        return this.attaque;
+    }
+
+    public int getDefense() {
+        return this.defense;
+    }
+
+    public String getArme() {  // doit retourner String, pas int
+        return this.Arme;
+    }
+
+    @Override
+    public String toString() {
+        return "Le personnage s'appelle " + firstname + " " + lastname + "\n" +
+                "PV : " + PV + " | PM : " + PM + "\n" +
+                "Attaque : " + attaque + " | Défense : " + defense + "\n" +
+                "Arme : " + Arme;
+    }
+}
---
+++ FireBones/composants/Personnage/testPersonnage.java
@@ -0,0 +1,6 @@
+public class testPersonnage{
+    public static void main(String[] args) {
+        Personnage p1 = new Personnage();
+        System.out.println(p1);
+    }
+}
