24f48c957fb07a91fc9a1c2dc8fd77d79878bf5c
Yanis Meziane <yanismeziane04@icloud.com>
2026-01-10 18:44:33 +0100

    Avancée du Main

--- FireBones/Main.java
+++ FireBones/Main.java
@@ -6,13 +6,61 @@ import composants.Personnage.Pretre;
 import composants.Monstre.Dragon;
 import composants.Monstre.Gobelin;
 import composants.Monstre.Vampire;
+import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("===Bienvenue à Firebones===");
+        System.out.println("===Bienvenue à Firebones===\n");
+        System.out.println("Sur les terres d'Arcus, vous devrez terrasser différents monstres pour libérer les villageois en détresse.\n");
+        System.out.println("Au cours de votre aventure vous combatrez différents monstres de plus en plus dur ! Donc soyez sur vos gardes et bonne chance \n");
 
+        //Initialisation du jeu
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Comment t'appelles-tu ? \n");
+        String firstname = scanner.nextLine();
+
+        System.out.println("Bienvenue " + firstname + " dans ces terre sacrées ! Votre objectif est de battre trois monstres qui sont venus envahir nos saintes terres ! Es-tu prêt.e.s à te lancer dans cette formidable aventure ?");
+
+        int choix;
+        boolean saisieValide = false;
+
+        do {
+            System.out.println("Possibilité : ");
+            System.out.println("1. Oui");
+            System.out.println("2. Non");
+            System.out.println("Veuillez choisir une option :");
+
+            try {
+                choix = scanner.nextInt();
+                if (choix == 1 || choix == 2) {
+                    saisieValide = true;
+                    switch(choix) {
+                        case 1:
+                            System.out.println("Très bon choix");
+                            break;
+
+                        case 2:
+                            System.out.println("N'hésites pas à venir tenter l'aventure quand le courage sera entre vos mains");
+                            break;
+
+                        default:
+                            System.out.println("Choix invalide ! Veuillez choisir une option valide");
+                            break;
+                    }
+                } else {
+                    System.out.println("Erreur de saisie ! Mettez 1 ou 2");
+                }
+            } catch(Exception e) {
+                System.out.println("Erreur de saisie de valeur. Entrez une valeur valide !");
+                scanner.nextLine();
+            }
+
+        } while (!saisieValide);
+
+        /*
         // Les personnages
 
         // Présentation Amazone
@@ -44,7 +92,8 @@ public class Main {
         Pretre pretre = Pretre.getPretre();
         System.out.println("\n === PRETRE === ");
         System.out.println(pretre);
-
+*/
+        /*
         // Les monstres
 
             // Présentation dragon
@@ -63,7 +112,7 @@ public class Main {
 
             Vampire vampire = Vampire.getVampire();
             System.out.println("\n === VAMPIRE === ");
-            System.out.println(vampire);
+            System.out.println(vampire);*/
 
 
     }
