51006b54d162735b93f0733628c1512feaaf1ea7
Yanis Meziane <yanismeziane04@icloud.com>
2026-02-16 19:03:08 +0100

    Fix some bugs

--- my-stats/frontend/src/Login/login.jsx
+++ my-stats/frontend/src/Login/login.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-export function Login(){
+export default function Login(){
     return(
         <div>
             <h1> Je suis la page Login </h1>
--- my-stats/frontend/src/Register/Register.jsx
+++ my-stats/frontend/src/Register/Register.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-export function Register(){
+export default function Register(){
     return(
         <div>
             <h1> Je suis la page Register </h1>
