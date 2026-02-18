a77db5abafdcd10c1ba3e753c463546cfbe1054d
Yanis Meziane <yanismeziane04@icloud.com>
2026-02-18 13:48:26 +0100

    Ajout de protection de routes

--- my-stats/frontend/src/App.js
+++ my-stats/frontend/src/App.js
@@ -4,17 +4,29 @@ import Register from "./Register/Register";
 import Login from "./Login/login";
 import Main from './Main/Main';
 import Admin from './Admin/Admin';
-
+import ProtectedRoute from './ProtectedRoutes';
+import Error from './Error/Error'; 
 function App() {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Register />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/Main' element={<Main />} />
-          <Route path='/Admin' element={<Admin />} />
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/error' element={<Error />} /> 
+
+        <Route path='/main' element={
+          <ProtectedRoute requiredRole="user">
+            <Main />
+          </ProtectedRoute>
+        } />
+
+        <Route path='/admin' element={
+          <ProtectedRoute requiredRole="admin">
+            <Admin />
+          </ProtectedRoute>
+        } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
