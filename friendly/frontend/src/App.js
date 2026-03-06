331ed03e49a4b50c34493d21115ce201219808e2
Yanis Meziane <yanismeziane04@icloud.com>
2026-03-06 11:28:33 +0100

    Initialisation Friendly

---
+++ friendly/frontend/src/App.js
@@ -0,0 +1,20 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// Importation de la page Main
+
+import Main from './Main/Main'
+
+
+function App(){
+  return(
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<Main />} />
+        </Routes>
+    </BrowserRouter>
+  )
+}
+
+
+export default App;
---
+++ friendly/frontend/src/Main/Main.jsx
@@ -0,0 +1,9 @@
+import './Main.css'
+
+export default function Main(){
+    return(
+        <div>
+            <h1>Je suis la page Main</h1>
+        </div>
+    )
+}
