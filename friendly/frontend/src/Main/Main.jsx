6d887051178e087de95c9232d551b5b2b7b6261d
Yanis Meziane <yanismeziane04@icloud.com>
2026-03-06 11:34:26 +0100

    Udpate Friendly

--- friendly/frontend/src/Main/Main.jsx
+++ friendly/frontend/src/Main/Main.jsx
@@ -4,6 +4,7 @@ export default function Main(){
     return(
         <div>
             <h1>Je suis la page Main</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium libero distinctio aperiam veniam beatae incidunt, alias rem quasi numquam nihil cumque suscipit esse ea repellat quisquam expedita magnam tenetur ad?</p>
         </div>
     )
 }
